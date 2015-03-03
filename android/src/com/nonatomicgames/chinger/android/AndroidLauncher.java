eb1836c8f1db6980180a03561e1a237e64bb5632
Mikhail Maslov <nonatomicgames@gmail.com>
2015-03-04 00:59:38 +0400

    Initial commit

---
+++ android/src/com/nonatomicgames/chinger/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nonatomicgames.chinger.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nonatomicgames.chinger.ChingerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChingerGame(), config);
+	}
+}
