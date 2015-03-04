10910ffeb5d0a5de747f63786131c744a886dc2e
Mikhail Maslov <nonatomicgames@gmail.com>
2015-03-04 22:08:00 +0400

    Size settings, deleted image

--- desktop/src/com/nonatomicgames/chinger/desktop/DesktopLauncher.java
+++ desktop/src/com/nonatomicgames/chinger/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.nonatomicgames.chinger.ChingerGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        // Sega Mega Drive sizes * 2;
+        config.width = 320 * 2;
+        config.height = 240 * 2;
+
 		new LwjglApplication(new ChingerGame(), config);
 	}
 }
