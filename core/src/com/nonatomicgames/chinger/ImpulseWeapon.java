78cfb5ed32913e7fd2f9d804f8bf8c99bcffcc24
Mikhail Maslov <nonatomicgames@gmail.com>
2015-03-22 22:16:15 +0400

    Impulse weapon prepared

---
+++ core/src/com/nonatomicgames/chinger/ImpulseWeapon.java
@@ -0,0 +1,26 @@
+package com.nonatomicgames.chinger;
+
+/**
+ * Created by MandM on 22.03.2015.
+ */
+public class ImpulseWeapon implements Weapon {
+    @Override
+    public boolean active() {
+        return false;
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+
+    @Override
+    public void found() {
+
+    }
+
+    @Override
+    public void lost() {
+
+    }
+}
