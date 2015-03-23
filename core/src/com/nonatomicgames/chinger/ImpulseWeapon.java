0a3cd4a85dc1758214a00218d91e109f3d859df3
Mikhail Maslov <nonatomicgames@gmail.com>
2015-03-23 23:52:06 +0400

    Impulse weapon updated

--- core/src/com/nonatomicgames/chinger/ImpulseWeapon.java
+++ core/src/com/nonatomicgames/chinger/ImpulseWeapon.java
@@ -4,23 +4,31 @@ package com.nonatomicgames.chinger;
  * Created by MandM on 22.03.2015.
  */
 public class ImpulseWeapon implements Weapon {
+
+    private Level level;
+    private boolean active;
+    private int numberOfBullets;
+    private Ship ship;
+
+    public ImpulseWeapon(Level level, Ship ship) {
+        this.level = level;
+        this.numberOfBullets = 0;
+    }
+
     @Override
     public boolean active() {
-        return false;
+        return numberOfBullets > 0;
     }
 
     @Override
     public void shoot() {
-
     }
 
     @Override
     public void found() {
-
+        numberOfBullets++;
     }
 
     @Override
-    public void lost() {
-
-    }
+    public void lost() {/*nothing*/}
 }
